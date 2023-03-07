c7d8331c69a231b1b4ec832c83b27ce5d2f3e74a
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2023-03-06 20:48:42 -0500

    editing names

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ map.on('click', 'neighbourhood-fill', (e) => {
 
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
-        .setHTML("<h5>" + name + "</h5>" + "Air Pollution PM 2.5 (μm) 2016 Level: " + level)
+        .setHTML("<h5>" + name + "</h5>" + "Air Pollution Find Particulate Matter (PM 2.5) 2016 Level: " + level)
         .addTo(map);
 });
 
